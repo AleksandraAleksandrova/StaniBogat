86ae4a27ee30150eaeea80c0f2db35febc06e331
Aleksandra Aleksandrova <AleksandraAleksandrova@users.noreply.github.com>
2021-06-07 22:51:11 +0300

    first steps in defining structures, needs modifying in the idea

--- main.c
+++ main.c
@@ -1,2 +1,24 @@
 printf("Hello World!");
 printf("Uspeshno promenqm main.c - Aleks");
+
+// trying some random things 07.06.2021
+#include <stdio.h>
+#include <stdlib.h>
+
+struct question_t {
+    // char[] name;
+    int difficulty;
+    
+}:
+
+struct answer_t{
+    char[] answer_text;
+    int if_right_answer;
+}:
+
+struct possible_answers_t {
+    struct answer_t a;
+    struct answer_t b;
+    struct answer_t c;
+    struct answer_t d;
+}
